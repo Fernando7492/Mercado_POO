10871b2932ca748ad434716e01c729ab07725791
andersonf007 <andersonfseabra3@gmail.com>
2022-09-10 11:22:31 -0300

    alterado a anotacao da classe

--- src/main/java/com/example/Mercado_POO/basica/Pessoa.java
+++ src/main/java/com/example/Mercado_POO/basica/Pessoa.java
@@ -7,9 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 
-@Entity
+@MappedSuperclass
 public class Pessoa {
 
 	@Id
